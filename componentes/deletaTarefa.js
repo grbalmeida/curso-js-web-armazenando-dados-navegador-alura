03ce053bbffe36b602ba0bcf71ca8f373b0ac376
gilvan <g.r.almeida@live.com>
2020-12-01 22:28:27 -0300

    Projeto final

---
+++ componentes/deletaTarefa.js
@@ -0,0 +1,21 @@
+const deletarTarefa = (atualiza, id) => {
+    const tarefasCadastradas = JSON.parse(localStorage.getItem('tarefas')) || []
+
+    tarefasCadastradas.splice(id, 1)
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefasCadastradas))
+
+    atualiza()
+}
+
+const BotaoDeleta = (atualiza, id) => {
+    const botaoDeleta = document.createElement('button')
+
+    botaoDeleta.innerText = 'Deletar'
+
+    botaoDeleta.addEventListener('click', () => deletarTarefa(atualiza, id))
+
+    return botaoDeleta
+}
+
+export default BotaoDeleta
